e9229a12babdeeb53c96f98291cee126da2644b9
Ramakrishna Koneru <Rama.Koneru@wnco.com>
2020-03-05 11:25:45 -0600

    Rama - Initial commit.

---
+++ src/main/java/com/rkoneru/fileuploaderservice/FileUploaderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkoneru.fileuploaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploaderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploaderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkoneru/fileuploaderservice/FileUploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkoneru.fileuploaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
